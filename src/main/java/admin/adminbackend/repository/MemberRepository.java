23f8d345ea426560b1085d8b7a48e37e2e7af4c2
Yooooonseo <81520018+Yooooonseo@users.noreply.github.com>
2024-04-05 00:08:02 +0900

    api 가져와서 json 반환 성공(DB에 저장해야함)

--- src/main/java/admin/adminbackend/repository/MemberRepository.java
+++ src/main/java/admin/adminbackend/repository/MemberRepository.java
@@ -1,8 +1,8 @@
-/*package admin.adminbackend.repository;
+package admin.adminbackend.repository;
 
 import admin.adminbackend.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,String> {
 
-}*/
+}
